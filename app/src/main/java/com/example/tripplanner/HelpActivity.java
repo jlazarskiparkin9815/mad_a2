5638c33247f5ca677ce4d472f1685ac258a8da37
Maxim Mikheev <mikheev.maxim@gmail.com>
2020-02-06 21:37:47 -0500

    Help Activity UI done, strings defined

--- app/src/main/java/com/example/tripplanner/HelpActivity.java
+++ app/src/main/java/com/example/tripplanner/HelpActivity.java
@@ -3,10 +3,21 @@ package com.example.tripplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HelpActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+
+public class HelpActivity extends AppCompatActivity implements TextWatcher {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +25,41 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // back button
 
-        Spinner helpTopicSelection = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(HelpActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.help_topics));
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        helpTopicSelection.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, getResources().getStringArray(R.array.help_topics));
+        AutoCompleteTextView editTextFilledExposedDropdown = findViewById(R.id.filled_exposed_dropdown);
+        editTextFilledExposedDropdown.setAdapter(adapter);
+        editTextFilledExposedDropdown.addTextChangedListener(this); // each time item selected, change textbox
+    }
+
+    public String findIndex(String[] checkArrr, String[] retArr, String match)
+    {
+        for (int i = 0; i < checkArrr.length; i++)
+        {
+            if (checkArrr[i].equals(match))
+            {
+                return retArr[i];
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public void afterTextChanged(Editable s)
+    {
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+    {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count)
+    {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(findIndex(getResources().getStringArray(R.array.help_topics), getResources().getStringArray(R.array.help_desc), s.toString()));
     }
 
     @Override
